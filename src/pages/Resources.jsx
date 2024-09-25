77c40ef7fa24f1c1701eb8a4b5a74b161438ad64
sadmaningithub <sadmanahmed261@gmail.com>
2024-09-25 14:47:58 +0600

    bg image setup complete for the resource search section on resources page

--- src/pages/Resources.jsx
+++ src/pages/Resources.jsx
@@ -12,8 +12,8 @@ const Resources = () => {
     return (
         <div>
 
-            <div className="text-center mt-10 space-y-7 bg-slate-300 p-10 rounded-xl">
-                <h1 className="text-3xl font-bold">What are you going to learn today?</h1>
+            <div className="text-center mt-10 space-y-7 bg-slate-300 p-4 lg:p-10 rounded-xl bg-[url('https://i.ibb.co.com/cJ0tskc/resource-Bg.jpg')] bg-no-repeat bg-cover bg-center">
+                <h1 className="text-3xl font-bold text-white">What are you going to learn today?</h1>
                 <div className='flex flex-row justify-center items-center ' >
                     <input type="text" placeholder="Type here" className="input input-bordered border-r-0 rounded-l-3xl rounded-r-none w-full max-w-md" />
                     <input className='btn  rounded-l-none rounded-r-3xl border-none bg-[#0096C7]' type="submit" value='Search' />
